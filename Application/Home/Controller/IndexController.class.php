7cc8dd3d2dc3975ec46b8c2387278910060e1d63
pongor <pongor_yuan@yeah.net>
2016-07-07 17:02:59 +0800

    a

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends AutuController {
     public function index(){ //103e688b8013ae4e53ecb3b67ae81e33
-        echo '学生端';
+        echo '学生端1111';
     }
 }
